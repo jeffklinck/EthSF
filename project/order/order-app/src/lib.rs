1ff6971fc90adfa81bf8f76da068d949b9301c5f
Panagiotis Michalopoulos <panm@pcloud.gr>
2024-10-20 01:06:08 -0400

    Write initial code for interacting with contract
    - Code for extracting state
    - Code for initializing order
    - Ignores owner state variable for now

--- project/order/order-app/src/lib.rs
+++ project/order/order-app/src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use anyhow::bail;
+use essential_types::{
+    solution::{Mutation, Solution, SolutionData},
+    PredicateAddress, Value, Word, ContentAddress,
+};
+
+const ORDER_AMOUNT_KEY: Word = 0;
+const PRICE_KEY: Word = 1;
+const TOKEN_A_KEY: Word = 2;
+const TOKEN_B_KEY: Word = 3;
+//const OWNER_KEY: Word = 4;
+
+#[derive(Clone)]
+pub struct Key(pub Vec<Word>);
+
+#[derive(Clone)]
+pub struct QueryState(pub Option<Value>);
+
+pub fn get_key(key: Word) -> Key {
+    Key(vec![key])
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn extract_state(state: QueryState) -> anyhow::Result<Word> {
+    match state.0 {
+        Some(state) => match &state[..] {
+            [] => Ok(0),
+            [state] => OK(*state),
+            _ => bail!("Expected single word, got: {:?}", state),
+        },
+        None => Ok(0),
+    }
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn create_bid(predicate: PredicateAddress, new_bid: Word, new_price: Word, new_A: ContentAddress, new_B: ContentAddress) -> Solution {
+    Solution {
+        data: vec![SolutionData {
+            predicate_to_solve: predicate,
+            decision_variables: Default::default(),
+            transient_data: Default::default(),
+            state_mutations: vec![Mutation {
+                key: vec![ORDER_AMOUNT_KEY],
+                value: vec![new_bid],
+            },
+            Mutation {
+                key: vec![PRICE_KEY],
+                value: vec![new_price],
+            },
+            Mutation {
+                key: vec![TOKEN_A_KEY],
+                value: vec![new_A],
+            },
+            Mutation {
+                key: vec![TOKEN_B_KEY],
+                value: vec![new_B],
+            }],
+        }],
     }
 }
