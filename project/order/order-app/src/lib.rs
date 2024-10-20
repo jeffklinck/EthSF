e1195e23b8bf7b1351e6bd529ee7c95515bb5216
Panagiotis Michalopoulos <panm@pcloud.gr>
2024-10-20 06:45:07 -0400

    Add decision variables in the solution and fix bugs

--- project/order/order-app/src/lib.rs
+++ project/order/order-app/src/lib.rs
@@ -1,30 +1,32 @@
 use anyhow::bail;
 use essential_types::{
     solution::{Mutation, Solution, SolutionData},
-    PredicateAddress, Value, Word, ContentAddress,
+    convert::word_4_from_u8_32,
+    PredicateAddress, Value, Word, ContentAddress, Key
 };
+use pint_abi;
 
-const ORDER_AMOUNT_KEY: Word = 0;
-const PRICE_KEY: Word = 1;
-const TOKEN_A_KEY: Word = 2;
-const TOKEN_B_KEY: Word = 3;
+pub const ORDER_AMOUNT_KEY: Word = 0;
+pub const PRICE_KEY: Word = 1;
+pub const TOKEN_A_KEY: Word = 2;
+pub const TOKEN_B_KEY: Word = 3;
 //const OWNER_KEY: Word = 4;
 
 #[derive(Clone)]
-pub struct Key(pub Vec<Word>);
+pub struct CustomKey(pub Vec<Word>);
 
 #[derive(Clone)]
 pub struct QueryState(pub Option<Value>);
 
-pub fn get_key(key: Word) -> Key {
-    Key(vec![key])
+pub fn get_key(key: Word) -> CustomKey {
+    CustomKey(vec![key])
 }
 
 pub fn extract_state(state: QueryState) -> anyhow::Result<Word> {
     match state.0 {
         Some(state) => match &state[..] {
             [] => Ok(0),
-            [state] => OK(*state),
+            [state] => Ok(*state),
             _ => bail!("Expected single word, got: {:?}", state),
         },
         None => Ok(0),
@@ -35,7 +37,7 @@ pub fn create_bid(predicate: PredicateAddress, new_bid: Word, new_price: Word, n
     Solution {
         data: vec![SolutionData {
             predicate_to_solve: predicate,
-            decision_variables: Default::default(),
+            decision_variables: vec![vec![new_bid], vec![new_price], word_4_from_u8_32(new_A.clone().0).to_vec(), word_4_from_u8_32(new_B.clone().0).to_vec()],
             transient_data: Default::default(),
             state_mutations: vec![Mutation {
                 key: vec![ORDER_AMOUNT_KEY],
@@ -47,11 +49,11 @@ pub fn create_bid(predicate: PredicateAddress, new_bid: Word, new_price: Word, n
             },
             Mutation {
                 key: vec![TOKEN_A_KEY],
-                value: vec![new_A],
+                value: word_4_from_u8_32(new_A.clone().0).to_vec(),
             },
             Mutation {
                 key: vec![TOKEN_B_KEY],
-                value: vec![new_B],
+                value: word_4_from_u8_32(new_A.clone().0).to_vec(),
             }],
         }],
     }
